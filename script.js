d0f18bec422c46c82f916bd98d633cf1ef5f070f
kmarczak-uni <120048656+kmarczak-uni@users.noreply.github.com>
2025-10-09 14:19:15 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('homeBtn').addEventListener('click', () => {
+    document.getElementById('content').innerHTML = '<p>This is the home page content.</p>';
+});
+
+document.getElementById('aboutBtn').addEventListener('click', () => {
+    document.getElementById('content').innerHTML = '<p>This is the about page content.</p>';
+});
